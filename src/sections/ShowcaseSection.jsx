fe61710255ec0d1c137b4b69cb040f91a9767eef
MaestroOG <muneeburrehman822@gmail.com>
2025-05-27 12:30:07 +0500

    update projects and project redirects

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -40,7 +40,7 @@ const ShowcaseSection = () => {
                 <div className="showcaselayout">
                     {/* LEFT:- */}
 
-                    <div className="first-project-wrapper" ref={project1Ref}>
+                    <a href="https://github.com/MaestroOG/feresApp" target="_blank" className="first-project-wrapper" ref={project1Ref}>
                         <div className="image-wrapper">
                             <img src="/images/project1.png" alt="Feres" width={840} height={212} />
                         </div>
@@ -48,24 +48,24 @@ const ShowcaseSection = () => {
                             <h2>Food Ordering Made Simple with a Powerful, User-Friendly App called Feres</h2>
                             <p className="text-white-50 md:text-xl">An app built with React, Redux Toolkit, & Tailwind CSS for a fast, user-friendly experience.</p>
                         </div>
-                    </div>
+                    </a>
 
                     {/* RIGHT:- */}
 
                     <div className="project-list-wrapper overflow-hidden">
-                        <div className="project" ref={project2Ref}>
+                        <a href="https://saas-maestro.vercel.app/" target="_blank" className="project" ref={project2Ref}>
                             <div className="image-wrapper bg-[#ffffff]">
-                                <img src="/images/project2.png" alt="Carhub" width={824} height={645} />
+                                <img src="/images/project2.png" alt="converso" width={824} height={645} />
                             </div>
-                            <h2>Carhub - A Car Showcase Site</h2>
-                        </div>
+                            <h2>Converso - Your Personalized AI Learning Partner</h2>
+                        </a>
 
-                        <div className="project" ref={project3Ref}>
+                        <a href="https://netflix-clone-navy-theta.vercel.app/" target="_blank" className="project" ref={project3Ref}>
                             <div className="image-wrapper bg-[#0e0c0c]">
                                 <img src="/images/project3.png" alt="Spotify Clone" width={824} height={645} />
                             </div>
-                            <h2>Spotify Clone - Made with React JS</h2>
-                        </div>
+                            <h2>Netflix Clone - Made with React & Firebase Auth</h2>
+                        </a>
                     </div>
                 </div>
             </div>
